8613953a214d22500bfb1fe9f5dabbb4102ce172
Amol Kushe <amolkushe@rediffmail.com>
2020-01-22 08:53:31 +0530

    First Commit

---
+++ GradleJava/src/main/java/GradleJava/Application.java
@@ -0,0 +1,11 @@
+package GradleJava;
+
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello World");
+	}
+
+}
